d3a4682a4127e792c4b09a4ac1ad6d52cdb0a66a
Bianca Antunes <62080345+biancaantunes@users.noreply.github.com>
2021-04-01 18:23:49 -0300

    Atividade

---
+++ Lista de Exercícios/ProjExercicio08032021/ProjExercicio08032021/Exercicio4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjExercicio08032021
+{
+    class Exercicio4
+    {
+        static void Main(string[] args)
+        {
+            double salario, comissao, vendas, totalvendas, salariofinal;
+
+            Console.Write("Digite o salário fixo: ");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a comissão de cada carro: ");
+            comissao = double.Parse(Console.ReadLine());
+            
+            Console.Write("Digite o número de carros vendidos: ");
+            vendas = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor total de vendas: " );
+            totalvendas = double.Parse(Console.ReadLine());
+            
+            salariofinal = salario + (comissao * vendas) + (totalvendas * 0.05);
+
+            Console.WriteLine("O salário final: " + salariofinal);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/*Uma revendedora de carros usados paga a seus funcionários vendedores um salário fixo por
+mês, mais uma comissão também fixa para cada carro vendido e mais 5% do valor das
+vendas por ele efetuadas. Escrever um algoritmo que leia o número de carros por ele
+vendidos, o valor total de suas vendas, o salário fixo e o valor que ele recebe por carro
+vendido. Calcule e escreva o salário final do vendedor.*/
---
+++ Lista de Exercícios/ProjExercicio08032021/ProjExercicio08032021/Exercicio5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjExercicio08032021
+{
+    class Exercicio5
+    {
+        static void Main(string[] args)
+        {
+            float altura, pesoideal;
+            string sexo;
+
+            Console.Write("Digite a sua altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o seu sexo (F/M): ");
+            sexo = string.Parse
+        }
+    }
+}
+
+/*Tendo como dados de entrada o nome, a altura e o sexo (M ou F) de uma pessoa, calcule e
+mostre seu peso ideal, utilizando as seguintes fórmulas:
+- para sexo masculino: peso ideal = (72.7 * altura) - 58
+- para sexo feminino: peso ideal = (62.1 * altura) - 44.7*/
