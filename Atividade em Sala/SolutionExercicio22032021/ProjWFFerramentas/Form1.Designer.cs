687205115d2c7711e4c910225de2afa555a1d279
Bianca Antunes <62080345+biancaantunes@users.noreply.github.com>
2021-04-13 23:12:45 -0300

    Atividade Ferramenta

---
+++ Atividade em Sala/SolutionExercicio22032021/ProjWFFerramentas/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace ProjWFFerramentas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblDescrição = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.dGVDados = new System.Windows.Forms.DataGridView();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnGerarTxt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(57, 27);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // lblDescrição
+            // 
+            this.lblDescrição.AutoSize = true;
+            this.lblDescrição.Location = new System.Drawing.Point(57, 91);
+            this.lblDescrição.Name = "lblDescrição";
+            this.lblDescrição.Size = new System.Drawing.Size(55, 13);
+            this.lblDescrição.TabIndex = 1;
+            this.lblDescrição.Text = "Descricao";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(61, 163);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(28, 13);
+            this.lblTipo.TabIndex = 2;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(61, 232);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 3;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Location = new System.Drawing.Point(63, 298);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(35, 13);
+            this.lblPreco.TabIndex = 4;
+            this.lblPreco.Text = "Preco";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(60, 58);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(167, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(60, 124);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(167, 20);
+            this.txtDescricao.TabIndex = 6;
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(64, 192);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(163, 20);
+            this.txtTipo.TabIndex = 7;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(64, 263);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(163, 20);
+            this.txtMarca.TabIndex = 8;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(64, 325);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(163, 20);
+            this.txtPreco.TabIndex = 9;
+            // 
+            // dGVDados
+            // 
+            this.dGVDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVDados.Location = new System.Drawing.Point(272, 49);
+            this.dGVDados.Name = "dGVDados";
+            this.dGVDados.Size = new System.Drawing.Size(240, 163);
+            this.dGVDados.TabIndex = 10;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(289, 288);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(114, 23);
+            this.btnSalvar.TabIndex = 11;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnGerarTxt
+            // 
+            this.btnGerarTxt.Location = new System.Drawing.Point(409, 288);
+            this.btnGerarTxt.Name = "btnGerarTxt";
+            this.btnGerarTxt.Size = new System.Drawing.Size(103, 23);
+            this.btnGerarTxt.TabIndex = 12;
+            this.btnGerarTxt.Text = "GerarTxt";
+            this.btnGerarTxt.UseVisualStyleBackColor = true;
+            this.btnGerarTxt.Click += new System.EventHandler(this.btnGerarTxt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 379);
+            this.Controls.Add(this.btnGerarTxt);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.dGVDados);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblDescrição);
+            this.Controls.Add(this.lblId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dGVDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblDescrição;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.DataGridView dGVDados;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnGerarTxt;
+    }
+}
+
